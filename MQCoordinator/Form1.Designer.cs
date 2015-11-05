04135a429de99afb5dbe0ee706e96bb73671f8ed
Hudson Akridge <hudson.akridge@vauto.com>
2015-11-04 18:22:48 -0600

    Added some cleaner behavior on form

--- MQCoordinator/Form1.Designer.cs
+++ MQCoordinator/Form1.Designer.cs
@@ -29,9 +29,11 @@
         private void InitializeComponent()
         {
             this.ux_LoadAndRun = new System.Windows.Forms.Button();
-            this.ux_pluginName = new System.Windows.Forms.TextBox();
             this.ux_RunPlugin = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.ux_QueueAllPlugins = new System.Windows.Forms.Button();
+            this.ux_QueueItemMessageBody = new System.Windows.Forms.TextBox();
+            this.ux_ListOfPlugins = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // ux_LoadAndRun
@@ -43,13 +45,6 @@
             this.ux_LoadAndRun.Text = "LoadAllPlugins";
             this.ux_LoadAndRun.UseVisualStyleBackColor = true;
             this.ux_LoadAndRun.Click += new System.EventHandler(this.ux_LoadAndRun_Click);
-            // 
-            // ux_pluginName
-            // 
-            this.ux_pluginName.Location = new System.Drawing.Point(22, 91);
-            this.ux_pluginName.Name = "ux_pluginName";
-            this.ux_pluginName.Size = new System.Drawing.Size(250, 20);
-            this.ux_pluginName.TabIndex = 1;
             // 
             // ux_RunPlugin
             // 
@@ -70,14 +65,41 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Plugin Name (Folder Name)";
             // 
+            // ux_QueueAllPlugins
+            // 
+            this.ux_QueueAllPlugins.Location = new System.Drawing.Point(25, 194);
+            this.ux_QueueAllPlugins.Name = "ux_QueueAllPlugins";
+            this.ux_QueueAllPlugins.Size = new System.Drawing.Size(181, 23);
+            this.ux_QueueAllPlugins.TabIndex = 4;
+            this.ux_QueueAllPlugins.Text = "Process Queue Item";
+            this.ux_QueueAllPlugins.UseVisualStyleBackColor = true;
+            // 
+            // ux_QueueItemMessageBody
+            // 
+            this.ux_QueueItemMessageBody.Location = new System.Drawing.Point(25, 168);
+            this.ux_QueueItemMessageBody.Name = "ux_QueueItemMessageBody";
+            this.ux_QueueItemMessageBody.Size = new System.Drawing.Size(247, 20);
+            this.ux_QueueItemMessageBody.TabIndex = 5;
+            this.ux_QueueItemMessageBody.Text = "Sarasota, FL";
+            // 
+            // ux_ListOfPlugins
+            // 
+            this.ux_ListOfPlugins.FormattingEnabled = true;
+            this.ux_ListOfPlugins.Location = new System.Drawing.Point(25, 90);
+            this.ux_ListOfPlugins.Name = "ux_ListOfPlugins";
+            this.ux_ListOfPlugins.Size = new System.Drawing.Size(247, 21);
+            this.ux_ListOfPlugins.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ux_ListOfPlugins);
+            this.Controls.Add(this.ux_QueueItemMessageBody);
+            this.Controls.Add(this.ux_QueueAllPlugins);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ux_RunPlugin);
-            this.Controls.Add(this.ux_pluginName);
             this.Controls.Add(this.ux_LoadAndRun);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -89,9 +111,11 @@
         #endregion
 
         private System.Windows.Forms.Button ux_LoadAndRun;
-        private System.Windows.Forms.TextBox ux_pluginName;
         private System.Windows.Forms.Button ux_RunPlugin;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ux_QueueAllPlugins;
+        private System.Windows.Forms.TextBox ux_QueueItemMessageBody;
+        private System.Windows.Forms.ComboBox ux_ListOfPlugins;
     }
 }
 
--- MQCoordinator/Form1.cs
+++ MQCoordinator/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 using MQCoordinator.Messaging;
 
@@ -14,11 +15,12 @@ namespace MQCoordinator
         private void ux_LoadAndRun_Click(object sender, EventArgs e)
         {
             PluginManager.LoadAllPlugins();
+            ux_ListOfPlugins.Items.AddRange(PluginManager.LoadedPlugins.Select(x => x.Key).ToArray());
         }
 
         private void ux_RunPlugin_Click(object sender, EventArgs e)
         {
-            var pluginName = ux_pluginName.Text;
+            var pluginName = ux_ListOfPlugins.Text;
             var plugin = PluginManager.GetPluginInstance(pluginName);
             plugin.HandleMessage(new KbbMessage("Sarasota, Florida"));
         }
